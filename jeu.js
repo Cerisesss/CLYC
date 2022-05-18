e6e11fae25132c677148038c583aa94eb844935d
PEDRA <PEDRA@intech-s1>
2022-05-18 15:02:29 +0200

    modif page de jeu principal css et js

--- jeu.js
+++ jeu.js
@@ -1,29 +1,5 @@
 "use strict";
 
-/*
-//récupérer un objet permettant de créer un objet"serveur HTTP"
-const http = require("http");
-const fs = require("fs");
-
-//sera appelée à l'arrivée de chaque requête
-const traite_requete = function (req, res) {
-
-	let contenu;
-	contenu = fs.readFileSync("modele_accueil_membre.html", "UTF-8");
-
-};
-
-//crée un serveur
-let mon_serveur = http.createServer(traite_requete);
-
-//démarre le serveur
-mon_serveur.listen(5000);
-
-
-*/
-
-
-
 const textElement = document.getElementById('text')
 const optionButtonsElement = document.getElementById('option-buttons')
 
@@ -66,135 +42,10 @@ function selectOption(option) {
 }
 
 
-const textNodes = [
-	{
-		id: 0,
-        text:"Vous commencez votre journée.",
-        options: [
-            {
-                text:"Se promener",
-                nextText:1
-            },
-            {
-                text:"Rester a la maison",
-                nextText:0
-            },  
-        ]
-	},
-	{
+html += '<img src = "' + situation[i].id + '.png">',
 
-		id: 1,
-		text:"John voit une grand-mère qui a besoin d'aide dans la rue",
-		options: [
-			{
-				text:"L'aider",
-				nextText:2
-			},
-			{
-			 	text:"Rien faire",
-                nextText:3
-			},
-			{
-			 	text:"L'insulter",
-                nextText:4
-			},
-		]
-	},
-	{
-		id: 2,
-        text:"La grand-mère vous remercie et vous donne 1M d'euros.",
-        options: [
-            {
-                text:"Accepter",
-                nextText:5
-            },
-            {
-                text:"Refuser",
-                nextText:6
-            },  
-            {
-                text:"Fuir",
-                nextText:7
-            },  
-        ]
-	},
-	{
-		id: 3,
-        text:"Vous vous sentez coupable.",
-        options: [
-            {
-                text:"Retouner l'aider",
-                nextText:2
-            },
-            {
-                text:"Rentrer à la maison",
-				nextText:0
-               
-			},
-        ]
-	},
-	{
-		id: 4,
-        text:"La grand-mère est furieuse! Les yakuza vous attaquent.",
-        options: [
-            {
-                text:"Vous rentrez chez vous en chaise roulante.",
-                nextText:0
-            },
-        ]
-	},
-	{
-		id: 5,
-        text:"C'était un rêve, vous vous réveillez en chaise roulante! (mdrr)" ,
-        options: [
-            {
-                text:"Commencer la journée.",
-                nextText:0
-            },
-        ]
-	},
-	{
-		id: 6,
-        text:"La grand-mère est impressionnnée. Elle vous offre un manoir (nouveau manoir)",
-        options: [
-            {
-                text:"Recommencer la journée",
-                nextText:0
-            },
-            {
-                text:"Quittez le jeu",
-                nextText:0
-            },
-        ]
-	},
-	{
-		id: 7,
-        text:"La grand-mère vous donne un coup de canne et vous lance un diamant sur la tête. Vous êtes maintenant riche!",
-        options: [
-            {
-                text:"Recommencer la journée",
-                nextText:0
-            },
-            {
-                text:"Quittez",
-                nextText:0
-            },    
-		]
-	}
-	]
 
+const textNodes = []
 
 startGame()
-
-
-
-
-
-
-
-
-
-
-
-
 
