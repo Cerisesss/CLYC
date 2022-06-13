ebdfe443e2e0398175c999ce98cdc2b108b502d3
PEDRA <PEDRA@intech-s1>
2022-06-13 14:51:10 +0200

    le jeu fonctionne git add jeuIndex.html req_choisir.js ! Passage des pages réussit

--- req_choisir.js
+++ req_choisir.js
@@ -9,24 +9,26 @@ const traite = function (req, res, query) {
 	let page;
     let contenu_fichier;
     let situation_jeu;
-	let situation; 
+	let situation;
 	let id;
 	let marqueurs;
 
-	
 	// query.situation // id de la situation précédente
-
-
+	
 	// ON LIT LES FICHIERS JSON EXISTANTS 
 
     contenu_fichier = fs.readFileSync("situation.json", 'utf-8');
-	situation = JSON.parse(contenu_fichier);
-    
+    situation = JSON.parse(contenu_fichier);
+
+	id = situation[query.situation].choix[query.rep].nextText;
+	
 	marqueurs = {};
+	marqueurs.situation = query.situation;
 	marqueurs.texte = monObjet.generer_texte (situation, id);
     marqueurs.buttons = monObjet.generer_button(situation[id].choix, id);
-        
-    page = fs.readFileSync("jeuIndex.html", 'utf-8');
+    marqueurs.temps = 180;
+    
+	page = fs.readFileSync("jeuIndex.html", 'utf-8');
     page = nunjucks.renderString(page, marqueurs);
 
  	res.writeHead(200, { 'Content-Type': 'text/html' });
