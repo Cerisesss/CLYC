09d6c80b02ae93adb0adedb53cfa490171334e81
DING_CELIVE <DING_CELIVE@intech-s1>
2022-06-15 13:28:02 +0200

    indice

--- jeu.js
+++ jeu.js
@@ -26,5 +26,41 @@ monObjet.generer_button = function (choix, id) {
     return marqueur;
 };
 
+
+
+
+
+
+//if : l affichage de ce qu'on veut mettre (icone)
+//dans situation.json : choisir selon notre adaptation les indices (honneur, bravour...)
+
+monObjet.generer_button = function (choix, id) {
+    let marqueur;
+	let contenu_situation;
+    let contenu_Femme;
+    let situation;
+    let Femme;
+
+    contenu_Femme = fs.readFileSync("Femme.json", 'utf-8');
+    Femme = JSON.parse(contenu_Femme);
+
+    marqueur = "";
+
+    for (let j = 0; j < choix.length; j++) {
+		if(honneur == true && Femme.length > 0){
+        	marqueur += `<a href = "/req_choisir?rep=${j}&situation=${id}"><button>`
+            	+ choix[j].text + choix[j].honneur + `</button></a>`;
+        	marqueur += `<br>`;
+			// situation[id].choix[j].honneur = "🏆";
+
+    	}else{
+			marqueur += `<a href = "/req_choisir?rep=${j}&situation=${id}"><button>`
+                + choix[j].text +`</button></a>`;
+            marqueur += `<br>`;
+	}
+    return marqueur;
+};
+
 module.exports = monObjet;
+
 
