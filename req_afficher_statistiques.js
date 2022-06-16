b9e980c6573c468cd019aad61ff69baf4a2b4353
MBAMI <MBAMI@intech-s1>
2022-06-16 11:33:30 +0200

    version finale de stats

--- req_afficher_statistiques.js
+++ req_afficher_statistiques.js
@@ -4,7 +4,7 @@ const nj = require("nunjucks");
 const generer_situation = require("./generer_situation.js");
 const statistiques  = function (req, res, query) {
     let contenu; // Contenu du fichier sous forme de chaîne de caractères
-    let Femme; // Contenu du fichier reconstruit en mémoire
+    let sauvegarde; // Contenu du fichier reconstruit en mémoire
     let html;
     let marqueurs;
     let page;
@@ -12,18 +12,47 @@ const statistiques  = function (req, res, query) {
     /* == Récupération des champs de la query-string == */
     /* == Récupération du contexte ==  */
     contenu = fs.readFileSync(query.pseudo + ".json", "utf-8");
-    query.pseudo = JSON.parse(contenu);
+    sauvegarde = JSON.parse(contenu); 
+	console.log(sauvegarde);
 
     /* == Traitement == */
 
     /* == Mémorisation du contexte == */
-    contenu = JSON.stringify(query.pseudo);
-    fs.writeFileSync(query.pseudo + ".json", contenu, "utf-8");
+    // contenu = JSON.stringify(query.pseudo);
+    // fs.writeFileSync(query.pseudo + ".json", contenu, "utf-8");
 
     /* == Fabrication et envoi de la réponse (page HTML) == */
     marqueurs = {};
-    marqueurs.photo = generer_situation((query.pseudo).fin_debloquees);
-	marqueurs.source = "images/photo1.jpg"
+	if (sauvegarde.fin_debloquees.indexOf("finA")!==-1) { 
+			marqueurs.source ="images/photo1.jpg"
+	} else {
+			marqueurs.source ="images/interrogation.jpg"	
+	}
+	if (sauvegarde.fin_debloquees.indexOf("finB")!==-1) { 
+			marqueurs.source_1 ="images/photo1.jpg"
+	} else {
+		marqueurs.source_1 = "images/interrogation.jpg"
+	}
+	if (sauvegarde.fin_debloquees.indexOf("finC")!==-1){
+			marqueurs.source_2 ="images/photo1.jpg"
+	} else {
+		marqueurs.source_2="images/interrogation.jpg"
+	}
+	if (sauvegarde.fin_debloquees.indexOf("finD")!==-1){
+    		marqueurs.source_3 ="images/photo1.jpg"
+    } else { 
+		marqueurs.source_3="images/interrogation.jpg"
+	}
+	if (sauvegarde.fin_debloquees.indexOf("finE")!==-1){
+    	marqueurs.source_4 ="images/photo1.jpg"
+    } else  {
+		marqueurs.source_4 ="images/interrogation.jpg"
+	}
+	if (sauvegarde.fin_debloquees.indexOf("finF")!==-1){
+   		 marqueurs.source_5 ="images/photo1.jpg"
+  	} else {
+		marqueurs.source_5 = "images/interrogation.jpg"
+	} 
 	marqueurs.pseudo = query.pseudo;
 	//console.log(marqueurs.liste);
     page = fs.readFileSync("statistiques.html", "utf-8");
