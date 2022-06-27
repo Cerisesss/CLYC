daef1184bb286e2830acaa2099d0ed648c8986c6
YE <YE@intech-s1>
2022-06-27 13:43:15 +0200

    idem

--- req_jouer.js
+++ req_jouer.js
@@ -12,7 +12,7 @@ const trait1 = function (req, res, query) {
 	let contenu;
 	let situation;
 	let marqueurs;
-	let temps = 61;
+	let temps = 121;
 
 	contenu = fs.readFileSync("situation.json", "utf-8");
 	situation = JSON.parse(contenu);
